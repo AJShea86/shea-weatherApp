d8a636eea7340a4d328886d37a2086698f890e7c
Andrew S <andrewsheamls@gmail.com>
2022-02-19 22:24:49 -0700

    got several things to show on the screen when searching for a city in the console

--- script.js
+++ script.js
@@ -34,9 +34,22 @@ var weather = {
         + this.apiKey
     )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => this.displayWeather(data));
 },
     displayWeather: function(data) {
+        var {name} = data;
+        var {icon, description} = data.weather[0];
+        var {temp, humidity,} = data.main;
+        var {speed} = data.wind
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector("#cityText").textContent = "Weather in " + name;
+        document.querySelector("#temperature").textContent = "Temperature " + temp;
+        document.querySelector("#humidity").textContent = "Humidity: " + humidity;
+        document.querySelector("#windSpeed").textContent = "windSpeed: " + speed;
+        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        // document.querySelector(".uvIndex").textContent = "Temperature" + uvIndex;
+
 
     }
 }
